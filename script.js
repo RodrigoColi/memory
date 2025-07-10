64a37035e1f4bc0b1bd1bba58001d19be1ada108
RodrigoColli <rodrigocollipedroso@gmail.com>
2025-07-10 13:57:26 -0300

    Mudando teclado para QWERTY

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ grid.addEventListener('click', async (e) => {
             }
             cards = [];
             cardsTurned = 0;
-        }, 1000);
+        }, 800);
     }
 });
 
@@ -210,9 +210,9 @@ function getName() {
         keyboard.classList.add('keyboard');
 
         const rows = [
-            ['A','B','C','D','E','F','G','H','I'],
-            ['J','K','L','M','N','O','P','Q','R'],
-            ['S','T','U','V','W','X','Y','Z']
+            ['Q','W','E','R','T','Y','U','I','O', 'P'],
+            ['A','S','D','F','G','H','J','K', 'L'],
+            ['Z','X','C','V','B','N','M']
         ];
 
         rows.forEach(row => {
